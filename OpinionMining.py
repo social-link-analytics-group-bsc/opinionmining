4fe08caf757d608a442966066184e74557bd2851
fabiocuri <fcuri91@gmail.com>
2019-10-30 11:59:57 +0000

    Update OpinionMining.py

--- OpinionMining.py
+++ OpinionMining.py
@@ -3,14 +3,9 @@
 
 import os
 import sys
-import csv
 import numpy as np
-from math import log
 from pandas import read_excel
 from collections import Counter
-from langdetect import detect_langs
-from numpy.random import RandomState
-from nltk.corpus import wordnet
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
@@ -18,14 +13,12 @@ from stop_words import get_stop_words
 from spacy.lang.es import Spanish
 
 def read_as_list(l, encoding):
-
     l_ = []
     with open(l, "rt", encoding=encoding) as f:
         l_ = f.read().splitlines()
     return l_
 
 def filter_vocabulary(l, pcg):
-
     vocab, words, l_filtered = [], [], []
     for l_ in l:
         for l__ in l_.split():
@@ -46,7 +39,6 @@ def filter_vocabulary(l, pcg):
     return l_filtered
 
 def tokenize(text, parser):
-    
     lda_tokens = []
     tokens = parser(text)
     for token in tokens:
@@ -58,11 +50,10 @@ def tokenize(text, parser):
             lda_tokens.append('SCREEN_NAME')
         else:
             lda_tokens.append(token.lower_)
-            
+
     return lda_tokens
 
 def prepare_text_for_ML(x, stop_words, parser, stemmer, to_avoid):
-
     tokens = tokenize(x, parser)
     tokens = [token for token in tokens if token not in stop_words]  # Remove stop-words.
     tokens = [x for x in tokens if not isinstance(x, int)]  # Remove integers.
@@ -78,29 +69,29 @@ if '__main__' == __name__:
     sys.setrecursionlimit(10000)
 
     cwd = os.getcwd()
-    
+
     stop_words = get_stop_words('es')
     parser = Spanish()
-    
-    #to_avoid = read_as_list('to_avoid.txt', 'latin-1')
+
+    # to_avoid = read_as_list('to_avoid.txt', 'latin-1')
     to_avoid = []
-    
+
     my_sheet = 'Sheet1'
-    file_name = 'Proposals - PAM - Spanish.xlsx' # name of your excel file
-    df = read_excel(file_name, sheet_name = my_sheet)
+    file_name = 'Proposals - PAM - Spanish.xlsx'  # name of your excel file
+    df = read_excel(file_name, sheet_name=my_sheet)
     df = df[df['category/name/se'] == 'Sanidad y salud']
-    
+
     txt = list(df['body'])
-    
+
     text = [filter_vocabulary(txt, 0.01)][0]
     text = [prepare_text_for_ML(x, stop_words, parser, stemmer, to_avoid) for x in text]
-    
+
     count_vect = CountVectorizer(max_df=0.8, min_df=2)
     doc_term_matrix = count_vect.fit_transform(text)
 
     LDA = LatentDirichletAllocation(n_components=5, random_state=42)
     LDA.fit(doc_term_matrix)
-    
-    for i,topic in enumerate(LDA.components_):
+
+    for i, topic in enumerate(LDA.components_):
         print(f'Topic #{i}:')
         print([count_vect.get_feature_names()[i] for i in topic.argsort()[-10:]])
